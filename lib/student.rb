14c703de098ac5fdfa949220c2dbf686b6170f25
Bobby Neal <rsneal01@gmail.com>
2020-09-13 17:09:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,8 +58,7 @@ class Student
   
   def self.new_from_db(row)
     sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
+    
   end
   
 end
