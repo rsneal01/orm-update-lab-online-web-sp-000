30a66760f248c2d9eaaa7f123a46a5b2cbddacc9
Bobby Neal <rsneal01@gmail.com>
2020-09-13 17:24:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,6 +57,12 @@ class Student
   end
   
   def self.new_from_db(row)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+    
+    
     new_student = self.new  
     new_student.id = row[0]
     new_student.name =  row[1]
